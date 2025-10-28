015369182b38204bad44902d4599cc26f0689d71
Gpober <gpober@iamcfo.com>
2025-10-28 03:07:09 -0400

    Delete lib/supabase.ts

--- lib/supabase.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://ebteqjhrdtxkuwizhqtz.supabase.co'
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVidGVxamhyZHR4a3V3aXpocXR6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk5MjM1MzAsImV4cCI6MjA3NTQ5OTUzMH0.ICUSi003DftHZ2Ts42hgFgndlwOJLGKnj_DmZFqHZ6w'
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
