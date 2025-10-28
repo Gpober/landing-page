978f5a6ab08611b65b0125d52ab16d855cca1ef7
Gpober <gpober@iamcfo.com>
2025-10-28 01:20:59 -0400

    Create postcss.config.js

---
+++ postcss.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
+```
+
+Commit.
+
+---
+
+### **File 6: `.gitignore`**
+Name: `.gitignore`
+```
+node_modules
+.next
+.env*.local
+.env
+.vercel
+*.tsbuildinfo
+next-env.d.ts
+.DS_Store
