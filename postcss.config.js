979dcad7488a5f847b143ceb904f5e81381fd883
Gpober <gpober@iamcfo.com>
2025-10-28 01:37:15 -0400

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -4,20 +4,3 @@ module.exports = {
     autoprefixer: {},
   },
 }
-```
-
-Commit.
-
----
-
-### **File 6: `.gitignore`**
-Name: `.gitignore`
-```
-node_modules
-.next
-.env*.local
-.env
-.vercel
-*.tsbuildinfo
-next-env.d.ts
-.DS_Store
